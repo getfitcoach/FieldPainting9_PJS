2ca2dcbad6d29ed77f1090e6bccfdd4529c02cfc
Paul <gowild45@gmail.com>
2020-01-06 13:39:16 -0500

    first commit

---
+++ fieldlinepainting9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fieldlinepainting9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of the circle that you are painting?");
+            string useranswer = Console.ReadLine();
+            double radius = Convert.ToDouble(useranswer);
+            double area = Math.PI * Math.Pow (radius, 2);
+            double buckets = Math.Ceiling (area / 100);
+    
+
+            Console.WriteLine ("You will need " + buckets + " buckets of paint.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
